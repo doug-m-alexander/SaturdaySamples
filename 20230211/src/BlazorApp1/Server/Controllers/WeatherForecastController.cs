89384d3f10d17f22902047928f985d4f4e05a7e9
Doug Alexander <trippedoutfish@gmail.com>
2023-02-11 21:34:58 -0800

    Replaying changes without apikey

--- 20230211/src/BlazorApp1/Server/Controllers/WeatherForecastController.cs
+++ 20230211/src/BlazorApp1/Server/Controllers/WeatherForecastController.cs
@@ -1,5 +1,11 @@
 using BlazorApp1.Shared;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.Xml;
+using System.Text.Json;
+using static System.Net.WebRequestMethods;
 
 namespace BlazorApp1.Server.Controllers
 {
@@ -10,7 +16,7 @@ namespace BlazorApp1.Server.Controllers
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
@@ -19,16 +25,40 @@ namespace BlazorApp1.Server.Controllers
             _logger = logger;
         }
 
+        //[HttpGet]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        //        TemperatureC = Random.Shared.Next(-20, 55),
+        //        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
+
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerator<WeatherApiCurrentDTO> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            // TODO: Replace with config values
+            string WeatherApiUrl = "http://api.weatherapi.com";
+            string WeatherApiVersion = "v1";
+            string apiKey = "";
+            string location = "92328";
+
+            string endpoint = $"{WeatherApiUrl}/{WeatherApiVersion}/current.json?key={apiKey}&q={location}&aqi=no";
+            var response = client.GetAsync(endpoint).Result;
+            WeatherApiCurrentDTO? deserializedObject;
+            if (response.IsSuccessStatusCode)
             {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+                var dataObject = response.Content.ReadAsStringAsync().Result;
+                deserializedObject = (WeatherApiCurrentDTO)JsonConvert.DeserializeObject(dataObject, typeof(WeatherApiCurrentDTO));
+            }
+
+            return null;
         }
     }
 }
---
+++ 20230211/src/BlazorApp1/Shared/WeatherApiCurrentDTO.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Shared
+{
+    public class WeatherApiCurrentDTO
+    {
+        public struct WeatherApiLocation
+        {
+            public string? name;
+            public string? region;
+            public string? country;
+            public double? lat;
+            public double? lon;
+            public string? tz_id;
+            public int? localtime_epoch;
+            public string? localtime;
+        }
+
+        public struct WeatherApiCurrentCondition
+        {
+            public string? text;
+            public string? icon;
+            public int? code;
+        }
+
+        public struct WeatherApiCurrent
+        {
+            public int? last_updated_epoch;
+            public DateTime last_updated;
+            public double? temp_c;
+            public double? temp_f;
+            public bool? is_day;
+            public WeatherApiCurrentCondition condition;
+            public double wind_mph;
+            public double wind_kph;
+            public int wind_degree;
+            public string? wind_dir;
+            public double? pressure_mb;
+            public double? pressure_in;
+            public double? precip_mm;
+            public double? precip_in;
+            public int? humidity;
+            public int? cloud;
+            public double? feelslike_c;
+            public double? feelslike_f;
+            public double? vis_km;
+            public double? vis_miles;
+            public double? uv;
+            public double? gust_mph;
+            public double? gust_kph;
+        }
+
+        public WeatherApiCurrentDTO()
+        {
+            location = new WeatherApiLocation();
+            current = new WeatherApiCurrent();
+        }
+
+        public WeatherApiLocation location;
+        public WeatherApiCurrent current;
+    }
+}
